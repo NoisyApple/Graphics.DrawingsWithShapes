1399b72ff456fb165d6d2f3969b20d360968ffd1
NoisyApple <leonel.aguirre77@gmail.com>
2020-10-12 16:48:36 -0500

    Project initialization.

---
+++ src/main/java/com/noisyapple/App.java
@@ -0,0 +1,13 @@
+package com.noisyapple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
